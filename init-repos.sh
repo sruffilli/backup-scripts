10e8f9217c495fb9e0e4565d0c660783ac82d710
Simone Ruffilli <ilruffo@gmail.com>
2022-10-11 20:08:20 +0200

    Update init-repos.sh

--- init-repos.sh
+++ init-repos.sh
@@ -4,7 +4,8 @@ export $(grep -v '^#' $CURRENT_PATH/.env | xargs -d '\n')
 
 IFS=',' read -ra RESTIC_REPOSITORIES <<< "$RESTIC_BACKENDS"
 for REPO in "${RESTIC_REPOSITORIES[@]}"; do
-  restic init --repo $REPO/mikrotik
-  restic init --repo $REPO/quarantanove_configs
-  restic init --repo $REPO/quarantanove_volumes
+  for LINE in `grep -v -E "^#|^\\s*$" $CURRENT_PATH/config.cfg` ; do
+    IFS="," read -ra REPO_CONFIG <<< "$LINE"
+    restic init --repo $REPO/${REPO_CONFIG[0]}
+  done
 done
